f06357fede7e445c462237a8018038a1365c20b3
Augusto Vicente <devguto@gmail.com>
2020-06-15 23:26:46 -0300

    progress

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,58 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {
-	Plugins,
-	PushNotification,
-	PushNotificationToken,
-	PushNotificationActionPerformed
-} from '@capacitor/core';
-const { PushNotifications } = Plugins;
+import { ModalController } from '@ionic/angular';
 @Component({
 	selector: 'app-explore-container',
 	templateUrl: './explore-container.component.html',
 	styleUrls: ['./explore-container.component.scss'],
 })
-export class ExploreContainerComponent implements OnInit
+export class ExploreContainerComponent
 {
-	@Input() name: string;
-	constructor() { }
-	ngOnInit()
+	@Input() url: string;
+	constructor(
+		public modalController: ModalController
+	) { }
+	closeModal()
 	{
-		console.log('Initializing HomePage');
-		// Request permission to use push notifications
-		// iOS will prompt user and return if they granted permission or not
-		// Android will just grant without prompting
-		PushNotifications.requestPermission().then(result =>
-		{
-			if (result.granted) {
-				// Register with Apple / Google to receive push via APNS/FCM
-				PushNotifications.register();
-			} else {
-				// Show some error
-			}
-		});
-		// On success, we should be able to receive notifications
-		PushNotifications.addListener('registration',
-			(token: PushNotificationToken) => {
-				alert('Push registration success, token: ' + token.value);
-			}
-		);
-		// Some issue with our setup and push will not work
-		PushNotifications.addListener('registrationError',
-			(error: any) => {
-				alert('Error on registration: ' + JSON.stringify(error));
-			}
-		);
-		// Show us the notification payload if the app is open on our device
-		PushNotifications.addListener('pushNotificationReceived',
-			(notification: PushNotification) => {
-				alert('Push received: ' + JSON.stringify(notification));
-			}
-		);
-		// Method called when tapping on a notification
-		PushNotifications.addListener('pushNotificationActionPerformed',
-			(notification: PushNotificationActionPerformed) => {
-				alert('Push action performed: ' + JSON.stringify(notification));
-			}
-		);
-	}
+        this.modalController.dismiss();
+    }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Servidor } from '../providers/server';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ModalController } from '@ionic/angular';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 @Component({
 	selector: 'app-tab1',
 	templateUrl: 'tab1.page.html',
@@ -9,25 +11,65 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class Tab1Page
 {
 	public videos:any = [];
+	public event:any = {};
 	public max:number = 3;
 	constructor(
-		server: Servidor,
-		private sanitizer: DomSanitizer
+		public server: Servidor,
+		private sanitizer: DomSanitizer,
+		public modalController: ModalController
 	)
 	{
-		let url = '?act=yt';
-		server.envia_get(url).subscribe((data:any)=>{
+		this.load_data();
+	}
+	load_data(e?)
+	{
+		// get youtube data
+		let url = '?act=yt',
+			get_event = ()=>
+			{
+				// get event data
+				url = '?act=event';
+				this.server.envia_get(url).subscribe((data:any)=>
+				{
+					this.event = data;
+					if(e) 
+						e.target.complete();
+				},(err)=>
+				{
+					console.log(err);
+				});
+			};
+		this.server.envia_get(url).subscribe((data:any)=>
+		{
 			this.videos = data.items;
-		},(err)=> {
-			console.log(err)
+			get_event();
+		},(err)=>
+		{
+			console.log(err);
+			get_event();
 		});
 	}
 	transform = (url) => this.sanitizer.bypassSecurityTrustResourceUrl(url);
-	loadData(e)
+	load_more(e)
 	{
 		setTimeout(() => {
-			e.target.complete();
 			this.max += 4;
 		}, 500);
+	}
+	open_url(url)
+	{
+		window.open(url, '_system', 'location=yes');
+	}
+	async play_video(url, title)
+	{
+		const modal = await this.modalController.create({
+			component: ExploreContainerComponent,
+			componentProps: {
+				'url': url,
+				'title': title
+			},
+			swipeToClose: true,
+		});
+		return await modal.present();
 	}
 }
